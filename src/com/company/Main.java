47194c479d54e36ef151babe0c6f723565b4e50d
diandika <mohammaddicky.andika@gmail.com>
2019-09-19 01:41:40 +0700

    extract attribute and function and its param from query

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,6 +1,7 @@
 package com.company;
 
 import com.company.QueryHandler.QueryHandler;
+import com.mongodb.*;
 
 import java.util.Scanner;
 import java.sql.*;
@@ -13,6 +14,14 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+
+        MongoClient mongoClient = new MongoClient(new MongoClientURI("mongodb://localhost:27017"));
+        DB db = mongoClient.getDB("tdrive");
+        DBCollection collection = db.getCollection("taxilog");
+        DBObject object = collection.find().one();
+
+        assert object != null;
+        System.out.println(object.get("lon").toString());
 
         String queryInput = input.nextLine();
         QueryHandler queryHandler = new QueryHandler();
--- src/com/company/QueryHandler/Parser/SQLOutput.java
+++ src/com/company/QueryHandler/Parser/SQLOutput.java
@@ -3,16 +3,36 @@ package com.company.QueryHandler.Parser;
 import com.company.QueryHandler.Parser.output.SqlBaseListener;
 import com.company.QueryHandler.Parser.output.SqlParser;
 
+import java.util.List;
+
 public class SQLOutput extends SqlBaseListener {
     public void enterParse(SqlParser.ParseContext context){
-        System.out.println("Entering Parse: "
-                + context.sql_stmt_list().get(0)
+        System.out.println("Entering Parse: ");
+        List<SqlParser.Result_columnContext> ctx = context.sql_stmt_list().get(0)
                 .sql_stmt().get(0)
                 .factored_select_stmt()
                 .select_core().get(0)
-                .result_column().get(2)
-                .getText()
-        );
+                .result_column();
+        for (SqlParser.Result_columnContext result_columnContext : ctx) {
+            printAttr(0, result_columnContext.expr());
+        }
+    }
+
+    private void printAttr(int i, SqlParser.ExprContext result_columnContext){
+        int n = i;
+        while (n>0){
+            System.out.print("    ");
+            n--;
+        }
+        if(result_columnContext.column_name() != null){
+            System.out.println(result_columnContext.getText());
+        }else if(result_columnContext.function_name() != null) {
+            System.out.println(result_columnContext.function_name().getText());
+            List<SqlParser.ExprContext> functionParameter = result_columnContext.expr();
+            for (SqlParser.ExprContext exprContext : functionParameter) {
+                printAttr(i+1, exprContext);
+            }
+        }
     }
 
     public void exitParse(SqlParser.ParseContext ctx ) {
